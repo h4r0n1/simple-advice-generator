51558dc628270d633cbe7617a7950584643c3a02
h4r0n1 <mdl.diop97@gmail.com>
2022-03-18 20:47:44 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function getAdvices() {
+  let API = "https://api.adviceslip.com/advice";
+  fetch(API)
+    .then((res) => res.json())
+    .then((data) => {
+      let result = data.slip;
+      console.log("My advices", result);
+      let id = document.querySelector("#advice__card__id");
+      id.innerHTML = `Advice #${result.id}`;
+
+      let text = document.querySelector("#advice__card__text");
+      text.innerHTML = result.advice;
+    });
+}
+
+getAdvices();
+// function renderAdvices() {
+//   let advices = getAdvices();
+
+//   // advices.forEach(advice => {
+
+//   // });
+
+// }
